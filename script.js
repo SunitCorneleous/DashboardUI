e7a9748afe6b17d215e664d5e6cad59ff8310a59
Sunit <sunit.corneleous25@gmail.com>
2024-05-24 19:12:35 +0600

    donut chart added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  console.log('jquery working');
+  //   console.log('jquery working');
 
   const weeklySalesData = {
     labels: [
@@ -36,4 +36,52 @@ $(document).ready(function () {
 
   const ctx = document.getElementById('weeklySalesChart').getContext('2d');
   new Chart(ctx, config);
+
+  const totalSalesData = {
+    labels: ['Electronics', 'Furniture', 'Groceries', 'Clothing', 'Toys'],
+    datasets: [
+      {
+        label: 'Total Sales',
+        data: [300, 150, 100, 200, 50],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const configdonut = {
+    type: 'doughnut',
+    data: totalSalesData,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        tooltip: {
+          callbacks: {
+            label: function (tooltipItem) {
+              return tooltipItem.label + ': ' + tooltipItem.raw;
+            },
+          },
+        },
+      },
+    },
+  };
+
+  const ctxdonut = document.getElementById('totalSalesChart').getContext('2d');
+  new Chart(ctxdonut, configdonut);
 });
